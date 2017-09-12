a3faea2bfe8e691ddb7e0aa1c342ae44e59d4387
Doug Neale <doug.neale@originenergy.com.au>
2017-09-12 15:50:34 +1000

    Added web pack html helper

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'dist/index.js',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js)$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', "stage-0", "react"]
-                }
-            }
-        ]
-    }, resolve: {
-        extensions: ['.js']
-    }
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.(js)$/,
+              loader: 'babel-loader',
+              query: {
+                  presets: ['es2015', "stage-0", "react"]
+              }
+          }
+      ]
+  }, resolve: {
+      extensions: ['.js']
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
