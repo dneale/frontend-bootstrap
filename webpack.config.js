32981bd9040d78c4f1b3c51afd30832824dd2011
Doug Neale <doug.neale@originenergy.com.au>
2017-09-12 15:12:42 +1000

    Basic webpack setup (WIP)

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'dist/index.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js)$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', "stage-0", "react"]
+                }
+            }
+        ]
+    }, resolve: {
+        extensions: ['.js']
+    }
+};
